98b4a89528c21a3ce072af80814d1c5ef445a764
renanrcs <renan.cavalcante@yahoo.com>
2022-11-12 10:44:36 -0300

    adjusts in the footer, skills and hobbies

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills = [
+    {imagem: "../../../assets/icons/skills/icon-html.svg",
+     alt: "Icone HTML",
+     titulo: "HTML 5"
+    },
+    {imagem: "../../../assets/icons/skills/icon-css.svg",
+     alt: "Icone CSS",
+     titulo: "CSS"
+    },
+    {imagem: "../../../assets/icons/skills/icon-react.svg",
+     alt: "Icone React",
+     titulo: "REACT"
+    },
+    {imagem: "../../../assets/icons/skills/icon-node.svg",
+     alt: "Icone Node",
+     titulo: "NODE"
+    },
+    {imagem: "../../../assets/icons/skills/icon-js.svg",
+     alt: "Icone JavaScript",
+     titulo: "JAVASCRIPT"
+    },
+    {imagem: "../../../assets/icons/skills/icon-php.svg",
+     alt: "Icone PHP",
+     titulo: "PHP"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
