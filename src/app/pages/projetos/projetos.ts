fc9dff03b2583d78686d158ba704b1c932cb46be
renanrcs <renan.cavalcante@yahoo.com>
2022-11-21 11:10:20 -0300

    add new project model

--- src/app/pages/projetos/projetos.ts
+++ src/app/pages/projetos/projetos.ts
@@ -6,6 +6,7 @@ export interface IProjetos{
 
 export const projetos: IProjetos[] = [
   { imagem: '../../../assets/images/img_challenge_decodificador.png', title: 'Decodificador de texto', subTitle: 'Challenge para criptografar palavras'},
-  { imagem: '../../../assets/images/img_challenge_jogoForca.png', title: 'Jogo da forca', subTitle: 'Challenge para criar um jogo da forca' }
+  { imagem: '../../../assets/images/img_challenge_jogoForca.png', title: 'Jogo da forca', subTitle: 'Challenge para criar um jogo da forca' },
+  { imagem: '../../../assets/images/img_projeto_Angular_Capgemini.png', title: 'Loja Virtual de vendas', subTitle: 'Curoso pela Capgemini com desenvolvimento em Angular' }
 ];
 
