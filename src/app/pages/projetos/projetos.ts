0a7e2a7d94b23d0f9efe9020fc140b602ebcad84
renanrcs <renan.cavalcante@yahoo.com>
2022-11-21 14:39:12 -0300

    images don't load

--- src/app/pages/projetos/projetos.ts
+++ src/app/pages/projetos/projetos.ts
@@ -7,8 +7,8 @@ export interface IProjetos{
 }
 
 export const projetos: IProjetos[] = [
-  { imagem: '../../../assets/images/img_challenge_decodificador.png', title: 'Decodificador de texto', subTitle: 'Challenge para criptografar palavras', repository: 'https://github.com/renanrcs/Challenge-ONE-crypthografy', demo: 'https://renanrcs.github.io/Challenge-ONE-crypthografy/'},
-  { imagem: '../../../assets/images/img_challenge_jogoForca.png', title: 'Jogo da forca', subTitle: 'Challenge para criar um jogo da forca', repository: 'https://github.com/renanrcs/Challenge-ONE-Hangman', demo: 'https://renanrcs.github.io/Challenge-ONE-Hangman/' },
-  { imagem: '../../../assets/images/img_projeto_Angular_Capgemini.png', title: 'Loja Virtual de vendas', subTitle: 'Curoso pela Capgemini com desenvolvimento em Angular', repository: 'https://github.com/renanrcs/Course_Angular_Capgemini', demo: 'https://renanrcs.github.io/Course_Angular_Capgemini/produtos' }
+  { imagem: './../../../assets/images/img_challenge_decodificador.png', title: 'Decodificador de texto', subTitle: 'Challenge para criptografar palavras', repository: 'https://github.com/renanrcs/Challenge-ONE-crypthografy', demo: 'https://renanrcs.github.io/Challenge-ONE-crypthografy/'},
+  { imagem: './../../../assets/images/img_challenge_jogoForca.png', title: 'Jogo da forca', subTitle: 'Challenge para criar um jogo da forca', repository: 'https://github.com/renanrcs/Challenge-ONE-Hangman', demo: 'https://renanrcs.github.io/Challenge-ONE-Hangman/' },
+  { imagem: './../../../assets/images/img_projeto_Angular_Capgemini.png', title: 'Loja Virtual de vendas', subTitle: 'Curoso pela Capgemini com desenvolvimento em Angular', repository: 'https://github.com/renanrcs/Course_Angular_Capgemini', demo: 'https://renanrcs.github.io/Course_Angular_Capgemini/produtos' }
 ];
 
--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -8,27 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
 
   skills = [
-    {imagem: "../../../assets/icons/skills/icon-html.svg",
+    {imagem: "./../../../assets/icons/skills/icon-html.svg",
      alt: "Icone HTML",
      titulo: "HTML 5"
     },
-    {imagem: "../../../assets/icons/skills/icon-css.svg",
+    {imagem: "./../../../assets/icons/skills/icon-css.svg",
      alt: "Icone CSS",
      titulo: "CSS"
     },
-    {imagem: "../../../assets/icons/skills/icon-angular.svg",
+    {imagem: "./../../../assets/icons/skills/icon-angular.svg",
      alt: "Icone Angular",
      titulo: "Angular"
     },
-    {imagem: "../../../assets/icons/skills/icon-spring.svg",
+    {imagem: "./../../../assets/icons/skills/icon-spring.svg",
      alt: "Icone Spring",
      titulo: "Spring"
     },
-    {imagem: "../../../assets/icons/skills/icon-javascript.svg",
+    {imagem: "./../../../assets/icons/skills/icon-javascript.svg",
      alt: "Icone JavaScript",
      titulo: "JAVASCRIPT"
     },
-    {imagem: "../../../assets/icons/skills/icon-java-original.svg",
+    {imagem: "./../../../assets/icons/skills/icon-java-original.svg",
      alt: "Icone Java",
      titulo: "Java"
     }
